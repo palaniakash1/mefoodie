b3edb9d9c0edeecd9780845493d79e938e87737a
Palani Akash <palaniakash1@gmail.com>
2025-11-12 01:07:27 +0530

    minor updation on header responsiveness

--- index.php
+++ index.php
@@ -45,6 +45,33 @@ $page_title = "MeFoodie - Home Page";
                 From Kitchen To Clicks <span class="text-tomato">Grow </span> Your Business Online!.
             </h1>
 
+
+
+            <!-- Search Bar -->
+            <div class="search-container-parent">
+                <div class="flex items-center bg-gray-100 rounded-full px-3 py-1 focus-within:ring-2 focus-within:ring-red-400 transition search-container">
+                    <input
+                        type="text"
+                        name="q"
+                        id="search-input"
+                        placeholder="Search location or tags..."
+                        class="bg-gray-100 flex-grow text-sm text-gray-700 focus:outline-none"
+                        autocomplete="off"
+                        required>
+                    <button type="button" id="search-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 text-gray-500 cursor-pointer"
+                            viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M13.293 14.707a8 8 0 111.414-1.414l3.387 3.387a1 1 0 01-1.414 1.414l-3.387-3.387zM8 14a6 6 0 100-12 6 6 0 000 12z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+                <div id="search-results" class="absolute bg-white w-full mt-2 rounded-xl shadow-lg hidden max-h-96 overflow-y-auto z-50"></div>
+            </div>
+
             <!-- business Grid -->
 
             <div id="business-list" class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 max-w-7xl mx-auto">
--- private/shared/header.php
+++ private/shared/header.php
@@ -2,45 +2,16 @@
 <link rel="stylesheet" href="<?php echo $base_url; ?>/public/stylesheets/style.css">
 <script src="https://cdn.tailwindcss.com"></script>
 
-<header class="bg-white shadow-md flex flex-col md:flex-row items-center justify-between px-6 py-2 sticky top-0 z-50 home-header sm-w-full">
-    <!-- Logo -->
-    <div class="flex items-start space-x-2 mb-2 md:mb-0 brand-name">
+<header class="bg-white shadow-md flex flex-col md:flex-row items-center justify-between px-6 py-2 sticky top-0 z-50 w-full home-header">
+    <div class="flex items-start space-x-2 mb-2 md:mb-0">
         <a href="<?php echo $base_url; ?>/index.php">
             <h1 class="text-2xl font-bold text-red-600 tracking-tight">MeFoodie</h1>
         </a>
     </div>
 
-
-    <!-- Search Bar -->
-    <div class="relative w-full md:w-1/2 search-container-parent" >
-        <div class="flex items-center bg-gray-100 rounded-full px-3 py-1 focus-within:ring-2 focus-within:ring-red-400 transition search-container">
-            <input
-                type="text"
-                name="q"
-                id="search-input"
-                placeholder="Search location or tags..."
-                class="bg-gray-100 flex-grow text-sm text-gray-700 focus:outline-none"
-                autocomplete="off"
-                required>
-            <button type="button" id="search-btn">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-500 cursor-pointer"
-                    viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M13.293 14.707a8 8 0 111.414-1.414l3.387 3.387a1 1 0 01-1.414 1.414l-3.387-3.387zM8 14a6 6 0 100-12 6 6 0 000 12z"
-                        clip-rule="evenodd" />
-                </svg>
-            </button>
-        </div>
-        <div id="search-results" class="absolute bg-white w-full mt-2 rounded-xl shadow-lg hidden max-h-96 overflow-y-auto z-50"></div>
-    </div>
-
-    <!-- Register Button -->
-    <button id="openPopupBtn" class="mt-2 md:mt-0 text-sm font-medium text-red-600 hover:text-red-800 transition reg-btn">
+    <button id="openPopupBtn" class="text-sm font-medium text-red-600 hover:text-red-800 transition reg-btn">
         Register your business
     </button>
-
 </header>
 
 <script>
